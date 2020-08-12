55e18b4e07499077f355299636af5c01a4d264aa
Karthik Vanka <karthikvanka97@gmail.com>
2020-08-12 13:09:32 +0530

    login form

--- devconnect-master/client/src/components/auth/Login.js
+++ devconnect-master/client/src/components/auth/Login.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export const Login = () => {
+
+    // formData = state
+    // setFormData = function to update the state
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+
+    const { email, password } = formData;
+
+    // [e.target.name] gets the name of the target 
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = async e => {
+        e.preventDefault();
+        console.log('SUCCESS');
+    }
+
     return (
-        <div>
-            Login
-        </div>
+        <>
+            <h1 className="large text-primary">Sign In</h1>
+            <p className="lead"><i className="fas fa-user"></i> Sign Into Your Account</p>
+            <form className="form" onSubmit={onSubmit}>
+                <div className="form-group">
+                    <input
+                        type="email"
+                        placeholder="Email Address"
+                        name="email"
+                        value={email}
+                        onChange={e => onChange(e)}
+                        required />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        minLength="6"
+                        autoComplete="on"
+                        value={password}
+                        onChange={e => onChange(e)}
+                    />
+                </div>
+
+                <input type="submit" className="btn btn-primary" value="Login" />
+            </form>
+            <p className="my-1">
+                Don't have an account? <Link to="/register">Sign Up</Link>
+            </p>
+        </>
     )
 }
