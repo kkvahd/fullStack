6fe773da2c0e380a3689c36c83ec06ceb64ac821
Karthik Vanka <karthikvanka97@gmail.com>
2020-08-13 13:13:31 +0530

    creating a redux store

--- devconnect-master/client/src/App.js
+++ devconnect-master/client/src/App.js
@@ -8,18 +8,25 @@ import { Landing } from './components/layout/Landing';
 import { Register } from './components/auth/Register';
 import { Login } from './components/auth/Login';
 
+// Redux
+// Wrap everything with provider so that all the elements inside it can access the store.
+import { Provider } from 'react-redux';
+import store from './store';
+
 const App = () =>
-  <Router>
-    <>
-      <Navbar />
-      <Route exact path='/' component={Landing} />
-      <section className='container'>
-        <Switch>
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/login' component={Login} />
-        </Switch>
-      </section>
-    </>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </section>
+      </>
+    </Router>
+  </Provider>
 
 export default App;
